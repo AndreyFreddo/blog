68a071b6775ce03a309d49d834786ed8e1e84c62
altshiftfive <71539080+altshiftfive@users.noreply.github.com>
2021-01-04 10:40:44 +0300

    Add files via upload

--- likes.js
+++ likes.js
@@ -4,8 +4,10 @@ let likesCounter = document.querySelector(".likes-counter");
 likesButton.onclick = function () {
   if (likesButton.classList.contains("added")) {
     likesCounter.textContent--;
+    likesButton.style.backgroundImage = "url(Content/Like.svg)";
   } else {
     likesCounter.textContent++;
+    likesButton.style.backgroundImage = "url(Content/Like-color.svg)";
   }
 
   likesButton.classList.toggle("added");
